697c1557c23c7ed125f42a5f65e8f85aabac8177
Logan Gelzer <lsgelzer7@gmail.com>
2021-11-18 11:36:00 -0500

    updates to project

--- assets/application.js
+++ assets/application.js
@@ -3,3 +3,40 @@ function toggleMenu () {
     let menu = document.querySelector('.menu-wrapper');
     menu.classList.toggle('open');
 }
+function toggleCart() {
+   
+    let menu = document.querySelector('.cart-drawer');
+    menu.classList.toggle('open');
+}
+function addToCart() {
+    let formData = {
+        'items': [{
+         'id': 42065672470773,
+         'quantity': 1
+         }]
+       };
+    fetch('/cart/add.js', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        }).then(
+            updateCart()
+       );
+}
+function updateCart() { 
+
+     fetch('/cart.js').then(response => 
+         response.json())
+    .then(
+        console.log(response.total_price)
+    )
+        
+}
